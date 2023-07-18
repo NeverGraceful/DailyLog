6197ad2ec6d5fca58ddcf231d4b6e887d2fb9af8
Anna Jimenea <ajimenea03@gmail.com>
2023-07-18 15:32:59 -0500

    ughhhh

--- bigBoy/main.py
+++ bigBoy/main.py
@@ -16,7 +16,8 @@ import math
 class MoodApp(QtWidgets.QMainWindow):
     #sets up UI, does the formatting for the canvas
     def __init__(self):
-        super(MoodApp, self).__init__()
+        super(MoodApp, self).__init__()       
+
         self.setWindowTitle("Daliy Log")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -42,13 +43,14 @@ class MoodApp(QtWidgets.QMainWindow):
         self.check_streak()
         self.ui.STREAK.setText("Streak: " + str(self.streak))
         self.ui.POINTS.setText("Points: " + str(self.user_points))
+        
 
     #once the user is ready, hits submit button - ends up here
     #saves the drawing as a .png and the text as a .txt
     def submitted(self):
         #add to our hashmap#
         today = date.today().strftime("%m-%d-%Y")
-        dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastText')
+        dir = os.path.join(self.find_path(),'pastText')
         file_name = dir +"\\" + today +".txt"
         today_file = Path(dir +"\\" + today + ".txt")
         if not today_file.is_file():
@@ -71,18 +73,20 @@ class MoodApp(QtWidgets.QMainWindow):
         box.exec_()
 
     def load_info(self):
-        wanted_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'info.txt')
+        wanted_path = os.path.join(self.find_path(),'info.txt')
+        # print("CURRENT CWD: "+str(os.getcwd()))
+        # os.chdir(Path(os.path.abspath(__file__) + "\\" + 'info.txt'))
         with open(wanted_path) as info_file:
             for line in info_file:
                 list = (re.split(':|\n', line))
                 print(list)
-                #TODO: change this to a switch case at some poind
-                if list[0] == "streak":
-                    self.streak = list[1]
-                elif list[0] == "points":
-                    self.user_points = list[1]
-                elif list[0] == "ava_hat":
-                    self.ava_hat = list[1]
+                match list[0]:
+                    case "streak":
+                        self.streak = list[1]
+                    case "points":
+                        self.user_points = list[1]
+                    case "ava_hat":
+                        self.ava_hat = list[1]
         info_file.close()
 
     def calulate_points_streak(self):
@@ -95,7 +99,7 @@ class MoodApp(QtWidgets.QMainWindow):
     def check_streak(self):
         yesterday = date.today() - timedelta(days=1)
         yesterday = yesterday.strftime("%m-%d-%Y")
-        wanted_path = Path(os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastText') +"\\" + yesterday + ".txt")
+        wanted_path = Path(os.path.join(self.find_path(),'pastText') +"\\" + yesterday + ".txt")
         if not wanted_path.is_file():
             #streak is broken awwwww man
             print("we are getting here! sad!: "+ str(wanted_path))
@@ -103,10 +107,19 @@ class MoodApp(QtWidgets.QMainWindow):
 
     def save_info(self):
         print("Saved")
-        wanted_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'info.txt')
+        wanted_path = os.path.join(self.find_path(),'info.txt')
         info_file = open(wanted_path, 'w')
         info_file.write("streak:" + str(self.streak) + "\nava_hat:"+ str(self.ava_hat) + "\npoints:"+ str(self.user_points) + "\n")
         info_file.close()
+
+    def find_path(self):
+        
+        if getattr(sys, 'frozen', False):
+            application_path = os.path.dirname(sys.executable)
+        else:
+           application_path = os.path.dirname(os.path.abspath(__file__))
+
+        return application_path
 
 # window that shows past entries  
 class PastWindow(QtWidgets.QDialog):
@@ -129,11 +142,13 @@ class PastWindow(QtWidgets.QDialog):
     #displays past entries
     def display_entry(self):
         selectedDate = self.ui.CALENDAR.selectedDate().toString("MM-dd-yyyy")
-        wanted_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastDrawings') +"\\" + selectedDate + ".png"
+        wanted_path = os.path.join(self.find_path(),'pastDrawings') +"\\" + selectedDate + ".png"
+        print("JUST CHECKING OMg: "+wanted_path)
         #checking if the file exists
         self.im = QPixmap(wanted_path)
         self.ui.PAST_DRAWING.setPixmap(self.im)
-        wanted_text_path =  os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastText') +"\\" + selectedDate + ".txt"
+        wanted_text_path =  os.path.join(self.find_path(),'pastText') +"\\" + selectedDate + ".txt"
+        print("past drawing path: "+ wanted_path)
         
         if not os.path.isfile(wanted_text_path):
             self.ui.PAST_TEXT.setText("No entry in storage")
@@ -148,7 +163,7 @@ class PastWindow(QtWidgets.QDialog):
 
     def export_drawing(self):
         selectedDate = self.ui.CALENDAR.selectedDate().toString("MM-dd-yyyy")
-        wanted_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastDrawings') +"\\" + selectedDate + ".png"
+        wanted_path = os.path.join(self.find_path(),'pastDrawings') +"\\" + selectedDate + ".png"
         file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save Image", "",
                          "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
         
@@ -160,16 +175,26 @@ class PastWindow(QtWidgets.QDialog):
 
     def export_text(self):
         selectedDate = self.ui.CALENDAR.selectedDate().toString("MM-dd-yyyy")
-        wanted_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'pastText') +"\\" + selectedDate + ".txt"
+        wanted_path = os.path.join(self.find_path(),'pastText') +"\\" + selectedDate + ".txt"
         file_name, _ =QtWidgets.QFileDialog.getSaveFileName(self, "Save Image", "",
                          "TXT(*.txt);;All Files(*.*) ")
         
         if file_name == "":
             return
 
-    
         shutil.copyfile(wanted_path, file_name)
 
+    def find_path(self):
+        print(os.path.dirname(sys.executable))
+        application_path = 0
+        if getattr(sys, 'frozen', False):
+            application_path = os.path.dirname(sys.executable)
+        else:
+            print("making it here lolol")
+            application_path = os.path.dirname(os.path.abspath(__file__))
+            print("application path "+ application_path)
+
+        return application_path
 
         
     def close_past(self):
